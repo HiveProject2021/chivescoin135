292a3ea971a67cc18a8869cdc0281d7eeb50dfc0
HiveProject <jwang123@nyit.edu>
2022-05-18 15:43:23 -0700

    Update farm_funcs.py

--- chives/cmds/farm_funcs.py
+++ chives/cmds/farm_funcs.py
@@ -278,3 +278,138 @@ async def summary(
             print("For details on farmed rewards and fees you should run 'chives wallet show'")
     else:
         print("Note: log into your key using 'chives wallet show' to see rewards for each key")
+
+
+async def uploadfarmerdata(rpc_port: int, wallet_rpc_port: int, harvester_rpc_port: int, farmer_rpc_port: int) -> None:
+    all_harvesters = await get_harvesters(farmer_rpc_port)
+    blockchain_state = await get_blockchain_state(rpc_port)
+    farmer_running = await is_farmer_running(farmer_rpc_port)
+
+    wallet_not_ready: bool = False
+    wallet_not_running: bool = False
+    amounts = None
+    try:
+        amounts = await get_wallets_stats(wallet_rpc_port)
+    except Exception as e:
+        if isinstance(e, aiohttp.ClientConnectorError):
+            wallet_not_running = True
+        else:
+            wallet_not_ready = True
+
+    FarmingStatus = ""
+    if blockchain_state is None:
+        FarmingStatus = "Not available"
+    elif blockchain_state["sync"]["sync_mode"]:
+        FarmingStatus = "Syncing"
+    elif not blockchain_state["sync"]["synced"]:
+        FarmingStatus = "Not synced or not connected to peers"
+    elif not farmer_running:
+        FarmingStatus = "Not running"
+    else:
+        FarmingStatus = "Farming"
+    
+    class PlotStats:
+        total_plot_size = 0
+        total_plots = 0
+        
+    if all_harvesters is not None:
+        harvesters_local: dict = {}
+        harvesters_remote: dict = {}
+        for harvester in all_harvesters["harvesters"]:
+            ip = harvester["connection"]["host"]
+            if is_localhost(ip):
+                harvesters_local[harvester["connection"]["node_id"]] = harvester
+            else:
+                if ip not in harvesters_remote:
+                    harvesters_remote[ip] = {}
+                harvesters_remote[ip][harvester["connection"]["node_id"]] = harvester
+
+        def process_harvesters(harvester_peers_in: dict):
+            for harvester_peer_id, plots in harvester_peers_in.items():
+                total_plot_size_harvester = sum(map(lambda x: x["file_size"], plots["plots"]))
+                PlotStats.total_plot_size += total_plot_size_harvester
+                PlotStats.total_plots += len(plots["plots"])
+                print(f"   {len(plots['plots'])} plots of size: {format_bytes(total_plot_size_harvester)}")
+
+        if len(harvesters_local) > 0:
+            print(f"Local Harvester{'s' if len(harvesters_local) > 1 else ''}")
+            process_harvesters(harvesters_local)
+        for harvester_ip, harvester_peers in harvesters_remote.items():
+            print(f"Remote Harvester{'s' if len(harvester_peers) > 1 else ''} for IP: {harvester_ip}")
+            process_harvesters(harvester_peers)
+
+        PlotCount = PlotStats.total_plots
+        TotalSizeOfPlots = format_bytes(PlotStats.total_plot_size)
+    else:
+        PlotCount = "Unknown"
+        TotalSizeOfPlots = "Unknown"
+
+    if blockchain_state is not None:
+        EstimatedNetworkSpace = format_bytes(blockchain_state["space"])
+    else:
+        EstimatedNetworkSpace = "Unknown"
+
+    minutes = -1
+    if blockchain_state is not None and all_harvesters is not None:
+        proportion = PlotStats.total_plot_size / blockchain_state["space"] if blockchain_state["space"] else -1
+        minutes = int((await get_average_block_time(rpc_port) / 60) / proportion) if proportion else -1
+
+    if all_harvesters is not None and PlotStats.total_plots == 0:
+        ExpectedTimeToWin = "Never (no plots)"
+    else:
+        ExpectedTimeToWin = format_minutes(minutes)
+
+    signage_points = await get_challenges(farmer_rpc_port)
+    if signage_points is None:
+        ""
+    else:
+        signage_points.reverse()
+        limit = 10
+        if limit != 0:
+            signage_points = signage_points[:limit]
+    
+    #get wallet address and fingerprint
+    from typing import List
+    from blspy import AugSchemeMPL, G1Element, G2Element
+    from chives.consensus.coinbase import create_puzzlehash_for_pk
+    from chives.util.bech32m import encode_puzzle_hash
+    from chives.util.config import load_config
+    from chives.util.default_root import DEFAULT_ROOT_PATH
+    from chives.util.ints import uint32
+    from chives.util.keychain import Keychain, bytes_to_mnemonic, generate_mnemonic
+    from chives.wallet.derive_keys import master_sk_to_farmer_sk, master_sk_to_pool_sk, master_sk_to_wallet_sk
+
+    root_path = DEFAULT_ROOT_PATH
+    config = load_config(root_path, "config.yaml")
+    keychain: Keychain = Keychain()
+    private_keys = keychain.get_all_private_keys()
+    selected = config["selected_network"]
+    prefix = config["network_overrides"]["config"][selected]["address_prefix"]
+    if len(private_keys) == 0:
+        FingerPrint = "No private keys"
+    else:
+        for sk, seed in private_keys:
+            FingerPrint = sk.get_g1().get_fingerprint()
+            FingerPrint = str(FingerPrint)[0:6]
+            Address = encode_puzzle_hash(create_puzzlehash_for_pk(master_sk_to_wallet_sk(sk, uint32(0)).get_g1()), prefix)
+            break
+    
+    from hashlib import sha256
+    import platform
+    RETURN_TEXT     = {}
+    RETURN_TEXT['MinerName'] = platform.node()
+    RETURN_TEXT['FingerPrint'] = FingerPrint
+    RETURN_TEXT['Address0'] = Address[0:45]
+    RETURN_TEXT['Address'] = sha256(Address[0:45].encode('utf-8')).hexdigest()
+    RETURN_TEXT['FarmingStatus'] = FarmingStatus
+    RETURN_TEXT['PlotCount'] = PlotCount
+    RETURN_TEXT['TotalSizeOfPlots'] = TotalSizeOfPlots
+    RETURN_TEXT['EstimatedNetworkSpace'] = EstimatedNetworkSpace
+    RETURN_TEXT['ExpectedTimeToWin'] = ExpectedTimeToWin
+    RETURN_TEXT['amounts'] = amounts
+    RETURN_TEXT['wallet_not_running'] = wallet_not_running
+    RETURN_TEXT['wallet_not_ready'] = wallet_not_ready
+    RETURN_TEXT['signage_points'] = signage_points
+    return RETURN_TEXT
+    #print(signage_points)
+    
