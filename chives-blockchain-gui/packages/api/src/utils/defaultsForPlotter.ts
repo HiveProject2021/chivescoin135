6e1c7d7dc3c1517ae71770fe5fe6cf8220ceba70
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2022-05-19 16:28:22 -0500

    remove bladebit

--- chives-blockchain-gui/packages/api/src/utils/defaultsForPlotter.ts
+++ chives-blockchain-gui/packages/api/src/utils/defaultsForPlotter.ts
@@ -1,11 +1,11 @@
 import PlotterName from '../constants/PlotterName';
 import { PlotterDefaults } from '../@types/Plotter';
-import { bladebitDefaults, madmaxDefaults, chiaposDefaults } from '../constants/Plotters';
+import { madmaxDefaults, chiaposDefaults } from '../constants/Plotters'; // bladebitDefaults,
 
 export default function defaultsForPlotter(plotterName: PlotterName): PlotterDefaults {
   switch (plotterName) {
-    case PlotterName.BLADEBIT:
-      return bladebitDefaults;
+    // case PlotterName.BLADEBIT:
+    //   return bladebitDefaults;
     case PlotterName.MADMAX:
       return madmaxDefaults;
     case PlotterName.CHIVESPOS: // fallthrough
--- chives-blockchain-gui/packages/api/src/utils/optionsForPlotter.ts
+++ chives-blockchain-gui/packages/api/src/utils/optionsForPlotter.ts
@@ -1,11 +1,11 @@
 import PlotterName from '../constants/PlotterName';
 import { PlotterOptions } from '../@types/Plotter';
-import { bladebitOptions, madmaxOptions, chiaposOptions } from '../constants/Plotters';
+import { madmaxOptions, chiaposOptions } from '../constants/Plotters'; // bladebitOptions, 
 
 export default function optionsForPlotter(plotterName: PlotterName): PlotterOptions {
   switch (plotterName) {
-    case PlotterName.BLADEBIT:
-      return bladebitOptions;
+    // case PlotterName.BLADEBIT:
+    //   return bladebitOptions;
     case PlotterName.MADMAX:
       return madmaxOptions;
     case PlotterName.CHIVESPOS: // fallthrough
